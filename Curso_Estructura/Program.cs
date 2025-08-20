e6abca1ae4dd3769114562c6c9f129fd2d0fc2f3
znecu <alma_maria@ucne.edu.do>
2025-08-19 20:56:21 -0400

    SegundaClase

--- Curso_Estructura/Program.cs
+++ Curso_Estructura/Program.cs
@@ -1,86 +1,81 @@
 ﻿namespace Curso_Estructura;
-
-class Program
+//condiciones
+public class Program
 {
     static void Main(string[] args)
     {
-        //suma de enteros
 
-        //entrada
-        int num1 = 5;
-        int num2 = 6;
+        //saber si es mayor de edadd
+        int edad = 18;
 
-        //proceso
-        int suma = num1 + num2;
+        if (edad >= 18)
+        {
+            System.Console.WriteLine("Eres mayor de edad");
+        }
+        else
+        {
+            System.Console.WriteLine("Eres menor de edad");
+        }
 
-        //salida
-        Console.WriteLine("La suma es igual a: " + suma);
+        //calificar notas
 
+        int nota = 89;
 
-        // Entrada via usuario
+        if (nota >= 90)
+        {
+            System.Console.WriteLine("ta pasao");
+        }
+        else if (nota >= 70)
+        {
+            System.Console.WriteLine("pasaste a chepa");
+        }
+        else
+        {
+            System.Console.WriteLine("ta quemao");
+        }
 
-        // Entrada
-        int numero1, numero2, totalSum;
+        //saber si un numero es neg o posi
 
-        Console.Write("Ingrese un primer numero: ");
-        numero1 = Convert.ToInt32(Console.ReadLine());
+        int numero;
+        System.Console.WriteLine("Ingrese un numero");
+        numero = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Ingrese un segundo numero: ");
-        numero2 = Convert.ToInt32(Console.ReadLine());
+        if (numero > 0)
+        {
+            System.Console.WriteLine("El numero es positivo");
 
-        // Proceso
-        totalSum = numero1 + numero2;
-
-        // Salida
-        Console.WriteLine("La suma es igual a: " + totalSum);
-
-
-        //resta de decimales
-        double a = 10.5;
-        double b = 5.5;
-
-        double resta = a - b;
-
-        Console.WriteLine("La resta es igual a: " + resta);
-
-        //multi de decimales
-        float c = 8.5f;
-        float d = 5.5f;
-
-        float multi = c * d;
-
-        Console.WriteLine("La multi es igual a: " + multi);
-
-
-        //Concatenar cadenas
-        string nombre = "Alma";
-        string Apellido = "Marte";
-
-        string nombreCompleto = nombre + " " + Apellido;
-
-        Console.WriteLine("Nombre completo: " + nombreCompleto);
+        }
+        else if (numero < 0)
+        {
+            System.Console.WriteLine("El numero es negativo");
+        }
+        else
+        {
+            System.Console.WriteLine("Cero");
+        }
 
 
-        //uso de char
-        char letra = 'A';
-        Console.WriteLine("La letra es: " + letra);
+        //edades
 
-        //uso de bool
-        int edad = 15;
-        bool mayorDeEdad = edad >= 18;
+        System.Console.WriteLine("Ingresa tu edad: ");
+        int edad1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Es mayor de edad? " + mayorDeEdad);
+        if (edad1 < 10)
+        {
+            System.Console.WriteLine("Eres un carajito");
+        }
+        else if (edad1 >= 10 && edad1 <= 18)
+        {
+            System.Console.WriteLine("Eres adolescente");
+        }
+        else if (edad1 >= 19 && edad1 <= 45)
+        {
+            System.Console.WriteLine("Eres adulto");
+        }
+        else
+        {
+            System.Console.WriteLine("Eres viejo");
+        }
 
-        // Operadores Logicos
-        int p = 7;
-
-        bool comprobarY = (p > 5) && (p < 10);
-        Console.WriteLine("Dame la comprobacion: " + comprobarY);
-
-        bool comprobarO = (p > 5) || (p < 6);
-        Console.WriteLine("Dame la comprobacion: " + comprobarO);
-
-        bool comprobarNegacion = !(p > 5);
-        Console.WriteLine("Dame la comprobacion: " + comprobarNegacion);
     }
 }
